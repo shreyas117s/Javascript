0ff9ab6188e14883ba4d46832fae80d29264a8d6
shreyas117s <shreyasdabke4669@gmail.com>
2023-12-29 19:17:33 +0530

    new file added

---
+++ Basic/02datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert(3 + 3) // we are using nodejs,not browser
+//it works in browser in that console
+
+console.log(3+3)
+
+console.log("Shreyas");
+
+let name = "Shreyas"
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null // null is a stand alone value or datatype,null value is emty
+// undefined => undefined means value is not defined
+// symbol // it is unique 
+// object 
+
+console.log(typeof "Shreyas"); //typeof function tell you which type of data is there or datatype//
+console.log(typeof null);
