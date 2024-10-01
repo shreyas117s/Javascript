a42b6a2b5d57d4d278ed9247907ceb51196a4899
Shreyas Dabke <shreyasdabke4669@gmail.com>
2024-10-01 08:22:48 -0700

    2 Variable

---
+++ variable.js
@@ -0,0 +1,14 @@
+console.log("This is Javascript  Course Playlist by Code with Harry")
+
+var a=67
+
+console.log(a)
+
+ a="Shreyas"
+
+ console.log(a)
+
+ //output shows 67 and Shreyas because Javascript is a Dynamically type language
+ //Javascript Allows to change Run Time
+
+ //let 8harry=7  is not work its Throw Error because its breaking the Rule Number caN NOT be First
